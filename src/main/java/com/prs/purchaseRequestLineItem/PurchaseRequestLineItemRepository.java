08a00908ced8a041dac40c4b4c1acaf7d6f8be15
mpage68 <mikepage68@live.com>
2018-10-29 13:58:06 -0400

    repository issue

--- src/main/java/com/prs/purchaseRequestLineItem/PurchaseRequestLineItemRepository.java
+++ src/main/java/com/prs/purchaseRequestLineItem/PurchaseRequestLineItemRepository.java
@@ -6,6 +6,6 @@ import org.springframework.data.repository.CrudRepository;
 
 public interface PurchaseRequestLineItemRepository extends CrudRepository<PurchaseRequestLineItem, Integer> {
 
-	List<PurchaseRequestLineItem> findAllByPurchaseRequestId(int id);
+	//List<PurchaseRequestLineItem> findAllByPurchaseRequestId(int id);
 
 }
--- src/main/java/com/prs/purchaserequest/PurchaseRequestRepository.java
+++ src/main/java/com/prs/purchaserequest/PurchaseRequestRepository.java
@@ -5,5 +5,5 @@ import java.util.List;
 import org.springframework.data.repository.CrudRepository;
 
 public interface PurchaseRequestRepository extends CrudRepository<PurchaseRequest, Integer> {
-List<PurchaseRequest> findAllByUserIdNotAndStatus(int id, String status);
+//List<PurchaseRequest> findAllByUserIdNotAndStatus(int iD, String status);
 }
